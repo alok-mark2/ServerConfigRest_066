3cee2a43f6985c838d46cbb127b4696f6c3f8e16
adham cahyo n <79594648+alubeban@users.noreply.github.com>
2022-01-03 19:20:12 +0700

    Initial commit

---
+++ ServerConfigRest_066_Adham Cahyo Nugroho/ServerConfigRest_066_Adham Cahyo Nugroho/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20190140066_AdhamCahyoNugroho;
+
+namespace ServerConfigRest_066_Adham_Cahyo_Nugroho
+{
+    static void Main(string[] args)
+    {
+        ServiceHost hostObjek = null;
+        try
+        {
+            hostObjek = new ServiceHost(typeof(TI_UMY));
+            hostObjek.Open();
+            Console.WriteLine("HHHH");
+            Console.ReadLine();
+            hostObjek.Close();
+        }
+        catch (Exception ex)
+        {
+            hostObjek = null;
+            Console.WriteLine(ex.Message);
+            Console.ReadLine();
+        }
+    }
+
+    internal class TI_UMY
+    {
+    }
+
+    internal class ServiceHost
+    {
+        internal void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Open()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
